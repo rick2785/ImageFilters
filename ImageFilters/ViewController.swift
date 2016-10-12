d7cecb754a2732b2118514988dddacedb56ee6be
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-10-11 20:28:12 -0500

    first commit

--- ImageFilters/ViewController.swift
+++ ImageFilters/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func filterAction(_ sender: Any) {
+        
+        guard let image = self.imageView.image?.cgImage else { return }
+        
+        let openGLContext = EAGLContext(api: .openGLES3) // Applied to the GPU
+        let context = CIContext(eaglContext: openGLContext!)
+        
+        let ciImage = CIImage(cgImage: image)
+        
+        let filter = CIFilter(name: "CIFalseColor") // CISepiaTone
+        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        //filter?.setValue(1, forKey: kCIInputIntensityKey)
+        
+        if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+            self.imageView?.image = UIImage(cgImage: context.createCGImage(output, from: output.extent)!)
+        }
+    }
 
 }
 
